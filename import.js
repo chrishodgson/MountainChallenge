f9fc74d024cf281f260c5e556cb714a5668184e9
Chris Hodgson <chris.3.hodgson@plus.net>
2018-11-12 10:22:38 +0000

    import

--- import.js
+++ import.js
@@ -15,48 +15,50 @@ mongoose.connect(
 
 const csvFilePath = args["filename"] || null;
 
-let count = 0;
+let count = 0; // todo how to output a count of the mountains imported
+
+const columns = /(Number|Name|Metres|Feet|Section|Area|Grid ref 10|Classification)/;
 
 const getJson = async () => {
-  //"/(Number|Name|Metres|Feet|Section|Area|Grid ref 10|Classification)/"
-  const jsonArray = await csv().fromFile(csvFilePath);
+  const jsonArray = await csv({'includeColumns':columns}).fromFile(csvFilePath);
   _.each(jsonArray, item => {
     const classifications = item["Classification"].split(",");
     if (classifications.includes("W")) {
       handleItem(item);
     }
   });
-  // todo get this working
-  // console.log("Mountains imported: " + count);
 };
 
 const handleItem = async item => {
   //todo a quicker find ??
   const existingMountain = await Mountain.find({ dobihId: item.Number });
 
-  if (!existingMountain.length) {
-    const mountain = new Mountain({
-      dobihId: item["Number"],
-      name: item["Name"],
-      lat: item["Latitude"],
-      lng: item["Longitude"],
-      metres: item["Metres"],
-      feet: item["Feet"],
-      section: item["Section"], //34A
-      area: item["Area"], //Lake District - Northern Fells
-      gridRef: item["Grid ref 10"]
-    });
-
+  if (existingMountain.length) {
+    console.log('Exists ' + item.Number + " // " + item.Name);
+  } else {
     try{
+      mountain = hydrateMountain(item);
       await mountain.save();
       console.log('Imported ' + item.Number + " // " + item.Name);
-      count++;
+      //count++;
     } catch(e) {
-      console.log(e, 'save error');
+      console.log(e, 'on save error');
     }
-  } else {
-    console.log('Exists ' + item.Number + " // " + item.Name);
   }
+};
+
+const hydrateMountain = item => {
+  return new Mountain({
+    dobihId: item["Number"],
+    name: item["Name"],
+    lat: item["Latitude"],
+    lng: item["Longitude"],
+    metres: item["Metres"],
+    feet: item["Feet"],
+    section: item["Section"], //34A
+    area: item["Area"], //Lake District - Northern Fells
+    gridRef: item["Grid ref 10"]
+  });
 };
 
 getJson();
