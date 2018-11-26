31c4ca62c42b4f1814f104b5044a7782fd6f1e87
Chris Hodgson <hello@chrishodgsonweb.co.uk>
2018-11-26 20:04:59 +0000

    importer

--- import.js
+++ import.js
@@ -1,8 +1,10 @@
 /**
  * import mountains from CSV
- * node import.js --filename=absoluteFilePath.csv --report --save
+ * node import.js --filename=absoluteFilePath.csv --report --save --country=E
  * switches: --save to save rows that don't already exist
  *           --report to report which rows exist and which do not
+ *           --classification classification to import
+ *           --country country to import
  *
  * countries
  * [ 'S', 'ES', 'M', 'W', 'E', 'C', 'I' ]
@@ -26,43 +28,43 @@ mongoose.connect(
 const Mountain = mongoose.model("mountains");
 const csvFilePath = args["filename"] || null;
 const reportItems = args["report"] || false;
+const country = args["country"] || false;
+const classification = args["classification"] || false;
 const saveItems = args["save"] || false;
 const columns = /(Number|Name|Metres|Feet|Area|Grid ref 10|Classification|Parent (Ma)|Map 1:25k|Country|County)/;
 
-const list = [];
-
 let existingCount = 0,
-  createdCount = 0;
+  createdCount = 0,
+  areas = [];
 
 const parseFile = async () => {
   const jsonArray = await csv({ includeColumns: columns }).fromFile(
     csvFilePath
   );
   for (const item of jsonArray) {
-    //restrict by classification
-    const classifications = item["Classification"].split(",");
-    if (classifications.includes("WO")) {
+    //import  by classification
+    const classificationList = item["Classification"].split(",");
+    if (classification && classificationList.includes(classification)) {
       await handleItem(item);
     }
 
-    //restrict by country
-    if(['C'].indexOf(item["Country"]) !== -1) {
-      //await handleItem(item);
+    //import by country
+    if (country && country === item["Country"]) {
+      await handleItem(item);
     }
-
-    //get a list of areas
-    // const string = item["Area"];
-    // if (string && list.indexOf(string) == -1) {
-    //   list.push(string);
-    // }
   }
+
+  if (saveItems && areas.length > 0) {
+    //todo save areas to db
+    console.log(areas, "areas");
+  }
+
   console.log(
     saveItems
       ? createdCount + " mountains created."
-      : createdCount + " mountains did not exist (not created)."
+      : createdCount + " mountains not found in database (not created)."
   );
-  console.log(existingCount + " mountains already exist.");
-  // console.log(list, "areas");
+  console.log(existingCount + " mountains already exist in database.");
 };
 
 const handleItem = async item => {
@@ -73,17 +75,23 @@ const handleItem = async item => {
   if (countDocuments > 0) {
     existingCount++;
     if (reportItems) {
-      console.log("Exists " + item.Country + " // " + item.Number + " // " + item.Name);
+      console.log("Exists " + item.Number + " // " + item.Name);
     }
   } else {
     try {
       createdCount++;
+
+      // add to list of areas
+      if (item["Area"] && areas.indexOf(item["Area"]) == -1) {
+        areas.push(item["Area"]);
+      }
+
       if (saveItems) {
         mountain = hydrateMountain(item);
         await mountain.save();
       }
       if (reportItems) {
-        console.log("Not Exists " + item.Country + " // " + item.Number + " // " + item.Name);
+        console.log("Not Exists " + item.Number + " // " + item.Name);
       }
     } catch (e) {
       console.log(e, "on save error");
