1b4c93bed06ad5788d4e85acdfbc8c72f82f95ce
Chris Hodgson <chris.3.hodgson@plus.net>
2018-12-07 13:34:21 +0000

    activities

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ChallengeList from "./challenges/ChallengeList";
 
 const Dashboard = () => {
   return (
     <div >
       <p>Dashboard</p>
-      <ChallengeList />
       <p><Link to="/challenges">List Challenges</Link></p>
       <p><Link to="/challenges/custom">New challenge - create your own list</Link></p>
       <p><Link to="/challenges/existing">New Challenge - existing list</Link></p>
