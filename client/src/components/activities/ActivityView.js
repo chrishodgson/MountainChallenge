b212b36eb82d34d4c03908f9bc4fe933fab168ee
Chris Hodgson <chris.3.hodgson@plus.net>
2018-12-10 13:19:51 +0000

    activities

--- client/src/components/activities/ActivityView.js
+++ client/src/components/activities/ActivityView.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import GoogleMapsContainer from "../GoogleMapsContainer";
 // import { fetchActivities } from "../../actions";
-import GoogleMap from "../GoogleMap";
+// import GoogleMap from "../GoogleMap";
+//<GoogleMap lon={-1.63081} lat={53.525688} />
 
 class ActivityView extends Component {
   // componentDidMount(props) {
@@ -9,40 +11,42 @@ class ActivityView extends Component {
   // }
 
   render() {
-    const item = this.props.activities[0];
-    return (
-      <table style={{ marginTop: "20px" }}>
-        <caption>Activity Details</caption>
-        <tbody>
-          <tr>
-            <th>Title</th>
-            <td>{item.title}</td>
-          </tr>
-          <tr>
-            <th>Desc</th>
-            <td>{item.description}</td>
-          </tr>
-          <tr>
-            <th>Count</th>
-            <td>{item.mountainCount}</td>
-          </tr>
-          <tr>
-            <th>Duration</th>
-            <td>{item.durationMinutes}</td>
-          </tr>
-          <tr>
-            <th>Date</th>
-            <td>{item.startDate}</td>
-          </tr>
-          <tr>
-            <th>Map</th>
-            <td>
-              <GoogleMap lon={-1.63081} lat={53.525688} />
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    );
+    return <GoogleMapsContainer/>;
+
+    // const item = this.props.activities[0];
+    // return (
+    //   <table style={{ marginTop: "20px" }}>
+    //     <caption>Activity Details</caption>
+    //     <tbody>
+    //       <tr>
+    //         <th>Title</th>
+    //         <td>{item.title}</td>
+    //       </tr>
+    //       <tr>
+    //         <th>Desc</th>
+    //         <td>{item.description}</td>
+    //       </tr>
+    //       <tr>
+    //         <th>Count</th>
+    //         <td>{item.mountainCount}</td>
+    //       </tr>
+    //       <tr>
+    //         <th>Duration</th>
+    //         <td>{item.durationMinutes}</td>
+    //       </tr>
+    //       <tr>
+    //         <th>Date</th>
+    //         <td>{item.startDate}</td>
+    //       </tr>
+    //       <tr>
+    //         <th>Map</th>
+    //         <td>
+    //             <GoogleMapsContainer/>
+    //         </td>
+    //       </tr>
+    //     </tbody>
+    //   </table>
+    // );
   }
 }
 
