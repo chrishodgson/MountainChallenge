b208c857224b22181387d748557a548e5a907e25
Chris Hodgson <hello@chrishodgsonweb.co.uk>
2018-12-21 08:38:00 +0000

    bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,15 @@
-import "materialize-css/dist/css/materialize.min.css";
+//import "materialize-css/dist/css/materialize.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reduxThunk from "redux-thunk";
-
+import axios from "axios";
 import App from "./components/App";
 import reducers from "./reducers";
 
-import axios from "axios";
 window.axios = axios; //todo remove
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
